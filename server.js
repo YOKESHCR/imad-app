c54d9e9f3f2cc2fb247bb20fb452049b4e67eef0
YOKESHCR <imcryk007@gmail.com>
2017-08-21 19:24:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('artcle-one',function(req,res){
+app.get('article-one',function(req,res){
     res.send('Article one will be sent and requested here!!');
 });
-app.get('artcle-two',function(req,res){
+app.get('article-two',function(req,res){
     res.send('Article 2 will be sent and requested here!!');
 });
 app.get('/ui/style.css', function (req, res) {
